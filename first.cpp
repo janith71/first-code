dc8ff0a0c76770c4e1d3fcc4d3ea29dad1a6b08e
unknown <janithdd021227@gmail.com>
2024-06-26 11:37:36 +0530

    hello world

---
+++ first.cpp
@@ -0,0 +1,15 @@
+/*
+first c++ code
+*/
+
+#include<iostream>
+
+using namespace std;
+int main ()
+{
+    cout << "hello world\n";
+    cout << "welcome to c plus!!!!";
+
+    return 0;
+    
+}    
